c8889a4560715b68beaa7fb342a5f61fd0034cc6
Yinkov <yinko.veliz@alumnos.ucn.cl>
2025-09-27 18:40:35 -0300

    Update: ITicketRepository

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 using MongoDB.Driver;
 using DotNetEnv;
+using perla_metro_ticket_service.Models;
+using perla_metro_ticket_service.Models.Enums;
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 
 string linkDb = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "mongodb+srv://<UserName>:<Contraseña>@base-ticket-service.y9rcn0b.mongodb.net/?retryWrites=true&w=majority&appName=Base-Ticket-Service";
-
+string nameDb = Environment.GetEnvironmentVariable("DATABASE_NAME") ?? "Data-base-ticket-service";
+string nameColection = Environment.GetEnvironmentVariable("COLECTION_TICKET_NAME") ?? "ticket";
 MongoClient mongoClient = new MongoClient(linkDb);
+
+var database1 = mongoClient.GetDatabase(nameDb);
+var collection = database1.GetCollection<Ticket>(nameColection);
+
+
+await collection.InsertOneAsync(new Ticket {
+    IdUser = "aaaaaa",
+    issueDate = DateTime.Now,
+    Type = TicketType.Ida,
+    State = TicketState.Activo,
+    Price = 1200.50m
+});
+
 
 List<String> databases = mongoClient.ListDatabaseNames().ToList();
 
--- src/Interfaces/ITicketRepository.cs
+++ src/Interfaces/ITicketRepository.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using perla_metro_ticket_service.Models;
 
 namespace perla_metro_ticket_service.src.Interfaces
 {
     public interface ITicketRepository
     {
-        
+        Task<Ticket> Add(Ticket ticket);
+        Task<List<Ticket>> GetAll();
+        Task<Ticket> GetById(string id);
+        Task<List<Ticket>> GetByIdUser(string idUser);
+        Task<bool> Update(Ticket ticket);
+        Task<bool> DeleteSoft(string id); 
     }
 }
