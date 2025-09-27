b48847cdca42b7b289bcee1b8c0eca3b494922ad
Yinkov <yinko.veliz@alumnos.ucn.cl>
2025-09-26 23:47:26 -0300

    feat: add model Ticket

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using perla_metro_ticket_service.Models.Enums;
 
 namespace perla_metro_ticket_service.Models
 {
     public class Ticket
     {
-        public ObjectId _id { get; set; }
-        public int IdUser { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public required string  IdUser { get; set; }
         public DateTime issueDate { get; set; }
         public TicketType Type { get; set; }
         public TicketState State { get; set; } = TicketState.Activo;
-        public float Price { get; set; }
+        public decimal Price { get; set; }
         public bool isActive { get; set; } = true;
-        public enum TicketState
-        {
-            Activo, Usado, Caducado
-        }
-        public enum TicketType
-        {
-            Ida,Vuelta
-        }
+ 
     }
 }
